3fc959d3f1523eaeb3eceb0fa8c18f269c2cb3f3
David Garay <garaekz@gmail.com>
2024-01-28 12:06:14 -0800

    Enable Shadcn vue

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,15 +1,25 @@
 <script setup>
-import AppLayout from '@/Layouts/AppLayout.vue';
+import MainLayout from '@/Layouts/MainLayout.vue';
 import Welcome from '@/Components/Welcome.vue';
 </script>
 
 <template>
-    <AppLayout title="Dashboard">
+    <MainLayout title="Dashboard">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
                 Dashboard
             </h2>
         </template>
+
+        <div class="border-b">
+            <div class="flex h-16 items-center px-4">
+                <MainNav class="mx-6" />
+                <div class="ml-auto flex items-center space-x-4">
+                <Search />
+                <UserNav />
+                </div>
+            </div>
+        </div>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -18,5 +28,5 @@ import Welcome from '@/Components/Welcome.vue';
                 </div>
             </div>
         </div>
-    </AppLayout>
+    </MainLayout>
 </template>
